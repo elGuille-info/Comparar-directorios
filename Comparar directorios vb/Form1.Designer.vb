80bb413937419f4fe35f90f1a3f0529e64f3db5f
Guillermo Som <71171321+elGuille-info@users.noreply.github.com>
2020-11-20 00:37:57 +0100

    Agregar archivos de proyecto.

---
+++ Comparar directorios vb/Form1.Designer.vb
@@ -0,0 +1,293 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim ListViewItem1 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem(New String() {"--", "generarClaveSHA1.sln", "19/11/2020 21:16:30"}, -1)
+        Dim ListViewItem2 As System.Windows.Forms.ListViewItem = New System.Windows.Forms.ListViewItem(New String() {"--", "generarClaveSHA1.sln", "19/11/2020 21:16:30"}, -1)
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.LabelIzq = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lvDirIzq = New System.Windows.Forms.ListView()
+        Me.chDir = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chNombre = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.chFecha = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.StatusStrip2 = New System.Windows.Forms.StatusStrip()
+        Me.LabelDer = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.lvDirDer = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ToolStripIzq = New System.Windows.Forms.ToolStrip()
+        Me.btnAbrirDirIzquierdo = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.btnComparar = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator3 = New System.Windows.Forms.ToolStripSeparator()
+        Me.btnLimpiar = New System.Windows.Forms.ToolStripButton()
+        Me.btnAbrirDirDerecho = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.StatusStrip1.SuspendLayout()
+        Me.StatusStrip2.SuspendLayout()
+        Me.ToolStripIzq.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 25)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.StatusStrip1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.lvDirIzq)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.StatusStrip2)
+        Me.SplitContainer1.Panel2.Controls.Add(Me.lvDirDer)
+        Me.SplitContainer1.Size = New System.Drawing.Size(800, 425)
+        Me.SplitContainer1.SplitterDistance = 400
+        Me.SplitContainer1.TabIndex = 0
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LabelIzq})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 403)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.ShowItemToolTips = True
+        Me.StatusStrip1.Size = New System.Drawing.Size(400, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'LabelIzq
+        '
+        Me.LabelIzq.Name = "LabelIzq"
+        Me.LabelIzq.Size = New System.Drawing.Size(385, 17)
+        Me.LabelIzq.Spring = True
+        Me.LabelIzq.Text = "ToolStripStatusLabel1"
+        Me.LabelIzq.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lvDirIzq
+        '
+        Me.lvDirIzq.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lvDirIzq.BackColor = System.Drawing.SystemColors.Info
+        Me.lvDirIzq.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.chDir, Me.chNombre, Me.chFecha})
+        Me.lvDirIzq.FullRowSelect = True
+        Me.lvDirIzq.GridLines = True
+        Me.lvDirIzq.HideSelection = False
+        ListViewItem1.StateImageIndex = 0
+        Me.lvDirIzq.Items.AddRange(New System.Windows.Forms.ListViewItem() {ListViewItem1})
+        Me.lvDirIzq.Location = New System.Drawing.Point(8, 3)
+        Me.lvDirIzq.MultiSelect = False
+        Me.lvDirIzq.Name = "lvDirIzq"
+        Me.lvDirIzq.ShowItemToolTips = True
+        Me.lvDirIzq.Size = New System.Drawing.Size(385, 397)
+        Me.lvDirIzq.TabIndex = 1
+        Me.lvDirIzq.UseCompatibleStateImageBehavior = False
+        Me.lvDirIzq.View = System.Windows.Forms.View.Details
+        '
+        'chDir
+        '
+        Me.chDir.Text = "Dir"
+        Me.chDir.Width = 34
+        '
+        'chNombre
+        '
+        Me.chNombre.Text = "Nombre"
+        Me.chNombre.Width = 204
+        '
+        'chFecha
+        '
+        Me.chFecha.Text = "Fecha / Hora"
+        Me.chFecha.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.chFecha.Width = 120
+        '
+        'StatusStrip2
+        '
+        Me.StatusStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LabelDer})
+        Me.StatusStrip2.Location = New System.Drawing.Point(0, 403)
+        Me.StatusStrip2.Name = "StatusStrip2"
+        Me.StatusStrip2.ShowItemToolTips = True
+        Me.StatusStrip2.Size = New System.Drawing.Size(396, 22)
+        Me.StatusStrip2.TabIndex = 3
+        Me.StatusStrip2.Text = "StatusStrip2"
+        '
+        'LabelDer
+        '
+        Me.LabelDer.Name = "LabelDer"
+        Me.LabelDer.Size = New System.Drawing.Size(381, 17)
+        Me.LabelDer.Spring = True
+        Me.LabelDer.Text = "ToolStripStatusLabel2"
+        Me.LabelDer.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'lvDirDer
+        '
+        Me.lvDirDer.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lvDirDer.BackColor = System.Drawing.SystemColors.Info
+        Me.lvDirDer.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3})
+        Me.lvDirDer.FullRowSelect = True
+        Me.lvDirDer.GridLines = True
+        Me.lvDirDer.HideSelection = False
+        ListViewItem2.StateImageIndex = 0
+        Me.lvDirDer.Items.AddRange(New System.Windows.Forms.ListViewItem() {ListViewItem2})
+        Me.lvDirDer.Location = New System.Drawing.Point(3, 3)
+        Me.lvDirDer.MultiSelect = False
+        Me.lvDirDer.Name = "lvDirDer"
+        Me.lvDirDer.ShowItemToolTips = True
+        Me.lvDirDer.Size = New System.Drawing.Size(385, 397)
+        Me.lvDirDer.TabIndex = 2
+        Me.lvDirDer.UseCompatibleStateImageBehavior = False
+        Me.lvDirDer.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Dir"
+        Me.ColumnHeader1.Width = 34
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Nombre"
+        Me.ColumnHeader2.Width = 204
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Fecha / Hora"
+        Me.ColumnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.ColumnHeader3.Width = 120
+        '
+        'ToolStripIzq
+        '
+        Me.ToolStripIzq.AutoSize = False
+        Me.ToolStripIzq.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.btnAbrirDirIzquierdo, Me.ToolStripSeparator1, Me.btnComparar, Me.ToolStripSeparator3, Me.btnLimpiar, Me.btnAbrirDirDerecho, Me.ToolStripSeparator2})
+        Me.ToolStripIzq.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStripIzq.Name = "ToolStripIzq"
+        Me.ToolStripIzq.Size = New System.Drawing.Size(800, 25)
+        Me.ToolStripIzq.TabIndex = 0
+        Me.ToolStripIzq.Text = "ToolStrip2"
+        '
+        'btnAbrirDirIzquierdo
+        '
+        Me.btnAbrirDirIzquierdo.Image = CType(resources.GetObject("btnAbrirDirIzquierdo.Image"), System.Drawing.Image)
+        Me.btnAbrirDirIzquierdo.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnAbrirDirIzquierdo.Name = "btnAbrirDirIzquierdo"
+        Me.btnAbrirDirIzquierdo.Size = New System.Drawing.Size(159, 22)
+        Me.btnAbrirDirIzquierdo.Text = "Abrir directorio Izquierdo"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'btnComparar
+        '
+        Me.btnComparar.Image = CType(resources.GetObject("btnComparar.Image"), System.Drawing.Image)
+        Me.btnComparar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnComparar.Name = "btnComparar"
+        Me.btnComparar.Size = New System.Drawing.Size(80, 22)
+        Me.btnComparar.Text = "Comparar"
+        Me.btnComparar.ToolTipText = "Comparar el contenido de los dos directorios"
+        '
+        'ToolStripSeparator3
+        '
+        Me.ToolStripSeparator3.Name = "ToolStripSeparator3"
+        Me.ToolStripSeparator3.Size = New System.Drawing.Size(6, 25)
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Image = CType(resources.GetObject("btnLimpiar.Image"), System.Drawing.Image)
+        Me.btnLimpiar.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(67, 22)
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.ToolTipText = "Limpiar la comparación"
+        '
+        'btnAbrirDirDerecho
+        '
+        Me.btnAbrirDirDerecho.Image = CType(resources.GetObject("btnAbrirDirDerecho.Image"), System.Drawing.Image)
+        Me.btnAbrirDirDerecho.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.btnAbrirDirDerecho.Name = "btnAbrirDirDerecho"
+        Me.btnAbrirDirDerecho.Size = New System.Drawing.Size(153, 22)
+        Me.btnAbrirDirDerecho.Text = "Abrir directorio derecho"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(6, 25)
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Controls.Add(Me.ToolStripIzq)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Mostrar el contenido de 2 directorios"
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        Me.SplitContainer1.Panel2.PerformLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.StatusStrip2.ResumeLayout(False)
+        Me.StatusStrip2.PerformLayout()
+        Me.ToolStripIzq.ResumeLayout(False)
+        Me.ToolStripIzq.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Private WithEvents ToolStripIzq As ToolStrip
+    Private WithEvents btnAbrirDirIzquierdo As ToolStripButton
+    Private WithEvents btnAbrirDirDerecho As ToolStripButton
+    Private WithEvents lvDirIzq As ListView
+    Private WithEvents chDir As ColumnHeader
+    Private WithEvents chNombre As ColumnHeader
+    Private WithEvents chFecha As ColumnHeader
+    Private WithEvents lvDirDer As ListView
+    Private WithEvents ColumnHeader1 As ColumnHeader
+    Private WithEvents ColumnHeader2 As ColumnHeader
+    Private WithEvents ColumnHeader3 As ColumnHeader
+    Private WithEvents SplitContainer1 As SplitContainer
+    Private WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Private WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Private WithEvents LabelIzq As ToolStripStatusLabel
+    Private WithEvents LabelDer As ToolStripStatusLabel
+    Private WithEvents StatusStrip1 As StatusStrip
+    Private WithEvents StatusStrip2 As StatusStrip
+    Private WithEvents btnComparar As ToolStripButton
+    Private WithEvents ToolStripSeparator3 As ToolStripSeparator
+    Private WithEvents btnLimpiar As ToolStripButton
+End Class
---
+++ Comparar directorios vb/Form1.vb
@@ -0,0 +1,170 @@
+﻿'------------------------------------------------------------------------------
+' Mostrar 2 paneles con ficheros 
+'
+'
+' (c) Guillermo (elGuille) Som, 2020
+'------------------------------------------------------------------------------
+Option Strict On
+Option Infer On
+
+Imports System
+'Imports System.Data
+Imports System.Collections.Generic
+Imports System.Text
+Imports System.Linq
+Imports Microsoft.VisualBasic
+Imports vb = Microsoft.VisualBasic
+Imports System.IO
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lvDirDer.Items.Clear()
+        lvDirIzq.Items.Clear()
+
+        Dim dirCfg = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+        Dim ficCfg As String
+
+        ficCfg = "Mostrar2Directorios_Izq.config.txt"
+        Dim fic = Path.Combine(dirCfg, ficCfg)
+        Dim dIzq As String
+        If File.Exists(fic) Then
+            Using sr As New StreamReader(fic, Encoding.Default, True)
+                dIzq = sr.ReadLine
+                sr.Close()
+            End Using
+
+            ' Mostrar los ficheros en el panel izquierdo
+            MostrarDirectorio(dIzq, lvDirIzq)
+        End If
+
+        ficCfg = "Mostrar2Directorios_Der.config.txt"
+        fic = Path.Combine(dirCfg, ficCfg)
+        If File.Exists(fic) Then
+            Using sr As New StreamReader(fic, Encoding.Default, True)
+                dIzq = sr.ReadLine
+                sr.Close()
+            End Using
+
+            ' Mostrar los ficheros en el panel derecho
+            MostrarDirectorio(dIzq, lvDirDer)
+        End If
+
+    End Sub
+
+    Private Sub BtnAbrirDirIzquierdo_Click(sender As Object, e As EventArgs) Handles btnAbrirDirIzquierdo.Click
+        AbrirCarpeta(lvDirIzq)
+    End Sub
+
+    Private Sub BtnAbrirDirDerecho_Click(sender As Object, e As EventArgs) Handles btnAbrirDirDerecho.Click
+        AbrirCarpeta(lvDirDer)
+    End Sub
+
+    Private Sub AbrirCarpeta(lv As ListView)
+
+        Dim sDir As String = ""
+        If lv.Tag IsNot Nothing Then
+            sDir = lv.Tag.ToString
+        End If
+        Dim fb As New FolderBrowserDialog With {
+            .Description = "Selecciona el directorio a mostrar",
+            .SelectedPath = Environment.SpecialFolder.MyDocuments.ToString
+        }
+        If sDir.Any Then
+            fb.SelectedPath = sDir
+        End If
+        If fb.ShowDialog() <> DialogResult.OK Then
+            Return
+        End If
+
+        ' Guardar el nombre del directorio abierto
+        Dim dirCfg = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+
+        Dim ficCfg As String
+        If lv Is lvDirIzq Then
+            ficCfg = "Mostrar2Directorios_Izq.config.txt"
+        Else
+            ficCfg = "Mostrar2Directorios_Der.config.txt"
+        End If
+        Dim fic = Path.Combine(dirCfg, ficCfg)
+        Using sw As New StreamWriter(fic, False, Encoding.Default)
+            sw.Write(fb.SelectedPath)
+            sw.Flush()
+            sw.Close()
+        End Using
+
+        MostrarDirectorio(fb.SelectedPath, lv)
+    End Sub
+
+    Private Sub MostrarDirectorio(sDir As String, lv As ListView)
+        lv.Tag = sDir
+        Dim dir = New DirectoryInfo(sDir)
+
+        If lv Is lvDirIzq Then
+            LabelIzq.Text = dir.FullName ' dir.Name
+            'LabelIzq.ToolTipText = dir.FullName
+        Else
+            LabelDer.Text = dir.FullName ' dir.Name
+            'LabelDer.ToolTipText = dir.FullName
+        End If
+
+        Dim dirs = dir.GetDirectories
+        Dim files = dir.GetFiles
+        lv.Items.Clear()
+        If dir.Parent IsNot Nothing Then
+            Dim it = lv.Items.Add("..")
+            it.BackColor = Color.Yellow
+            it.Tag = dir.Parent.FullName
+            it.Checked = False
+            it.ToolTipText = dir.Parent.FullName
+        End If
+        For Each di In dirs
+            Dim it = lv.Items.Add("DIR")
+            it.SubItems.Add(di.Name)
+            it.SubItems.Add(di.LastWriteTime.ToString("dd/MM/yyyy HH:mm:ss"))
+            it.ForeColor = Color.DarkOliveGreen
+            it.BackColor = Color.LightGoldenrodYellow
+            it.Checked = False
+            it.Tag = di.FullName
+            it.ToolTipText = di.FullName
+            'it.Font = New Font(it.Font, FontStyle.Bold)
+        Next
+        For Each fi In files
+            Dim it = lv.Items.Add("")
+            it.SubItems.Add(fi.Name)
+            it.SubItems.Add(fi.LastWriteTime.ToString("dd/MM/yyyy HH:mm:ss"))
+            it.Checked = False
+            it.Tag = Nothing
+        Next
+
+    End Sub
+
+    Private Sub LvDirIzq_DoubleClick(sender As Object, e As EventArgs) Handles lvDirIzq.DoubleClick, lvDirIzq.Click
+        IrParentDir(lvDirIzq)
+    End Sub
+
+    Private Sub LvDirDer_DoubleClick(sender As Object, e As EventArgs) Handles lvDirDer.DoubleClick, lvDirDer.Click
+        IrParentDir(lvDirDer)
+    End Sub
+
+    Private Sub IrParentDir(lv As ListView)
+        ' Comprobar si es un elemento con directorio
+        If lv.SelectedItems.Count > 0 Then
+            ' ir a ese directorio
+            Dim sDir = lv.SelectedItems(0).Tag.ToString
+            If Not String.IsNullOrEmpty(sDir) Then
+                MostrarDirectorio(sDir, lv)
+            End If
+        End If
+    End Sub
+
+    Private Sub BtnComparar_Click(sender As Object, e As EventArgs) Handles btnComparar.Click
+        ' Comparar el contenido de los 2 directorios
+
+    End Sub
+
+    Private Sub BtnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        MostrarDirectorio(lvDirIzq.Tag.ToString, lvDirIzq)
+        MostrarDirectorio(lvDirDer.Tag.ToString, lvDirDer)
+    End Sub
+End Class
